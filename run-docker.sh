0ae95b8f1a0c38d844722091712dc9b84dcbdb44
David Moles <david.moles@ucop.edu>
2018-10-17 11:45:50 -0700

    Modify startup script for skipping setup wizard, no-auth login

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LC_CTYPE=en_US.utf8 # just to be sure
 
 ADMIN_PASSWORD_PATH="/var/jenkins_home/secrets/initialAdminPassword"
 
@@ -15,15 +16,13 @@ To restart after stop: docker start ${CONTAINER_ID}
 To remove the container after stop: docker rm ${CONTAINER_ID}
 To log into the container as jenkins: docker exec -u jenkins -it ${CONTAINER_ID} /bin/bash
 To log into the container as root: docker exec -u root -it ${CONTAINER_ID} /bin/bash
-Waiting for Jenkins to start (should be ≲ 30 seconds)...
+Waiting for Jenkins to start (should be ≈ 10 seconds)...
 EOF
 
-docker exec -it ${CONTAINER_ID} bash -c "while [ ! -f ${ADMIN_PASSWORD_PATH} ]; do echo -n '.'; sleep 1; done; echo"
-
-JENKINS_ADMIN_PASSWORD=$(docker exec -it ${CONTAINER_ID} cat ${ADMIN_PASSWORD_PATH})
-[[ -z "${JENKINS_ADMIN_PASSWORD}" ]] && { echo "Unable to determine Jenkins admin password" ; exit 1; }
+STARTUP_DETECTION_PATH="/var/jenkins_home/logs"
+docker exec -it ${CONTAINER_ID} bash -c "while [ ! -d ${STARTUP_DETECTION_PATH} ]; do echo -n '.'; sleep 1; done; echo"
 
 cat <<EOF
-Jenkins running at http://${JENKINS_PORT} with admin password: ${JENKINS_ADMIN_PASSWORD}
+Jenkins running at http://${JENKINS_PORT}
 EOF
 
