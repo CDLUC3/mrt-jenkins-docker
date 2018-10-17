7d2bd2874897e221db946b10f238a3d1957704d2
David Moles <david.moles@ucop.edu>
2018-10-17 11:16:54 -0700

    Show JUnit failures

--- init.groovy.d/startup.groovy
+++ init.groovy.d/startup.groovy
@@ -6,13 +6,9 @@ Logger.global.info("Running startup.groovy...")
 
 Jenkins.instance.save() // TODO: what does this do?
 
-def job = Jenkins.instance.getJob('seed')
-Jenkins.instance.queue.schedule(job, 0, new CauseAction(new Cause() {
-  @Override
-  String getShortDescription() {
-    'Running startup.groovy'
-  }
-}))
-
+Jenkins.instance.queue.schedule(
+  Jenkins.instance.getJob('seed'),
+  0,
+  new RemoteCause("localhost", "Running startup.groovy")
+)
 Logger.global.info("...startup.groovy complete.")
-
