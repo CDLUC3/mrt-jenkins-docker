02154d5be05e2c1fa7ea0c3888f6c8e9196b571d
David Moles <david.moles@ucop.edu>
2019-03-06 09:19:25 -0800

    Document docker rm -f

--- run-docker.sh
+++ run-docker.sh
@@ -11,12 +11,15 @@ JENKINS_PORT=$(docker port ${CONTAINER_ID} 8080)
 
 cat <<EOF
 Jenkins Docker container running with container ID ${CONTAINER_ID}
-To stop the server: docker kill ${CONTAINER_ID}
-To restart after stop: docker start ${CONTAINER_ID}
-To remove the container after stop: docker rm ${CONTAINER_ID}
-To log into the container as jenkins: docker exec -u jenkins -it ${CONTAINER_ID} /bin/bash
-To log into the container as root: docker exec -u root -it ${CONTAINER_ID} /bin/bash
-To see output from the Jenkins startup command: docker logs ${CONTAINER_ID}
+
+- To stop the server:                   docker kill ${CONTAINER_ID}
+- To restart after stop:                docker start ${CONTAINER_ID}
+- To remove the container after stop:   docker rm ${CONTAINER_ID}
+- To stop and remove in one step:       docker rm -f ${CONTAINER_ID}
+- To log into the container as jenkins: docker exec -u jenkins -it ${CONTAINER_ID} /bin/bash
+- To log into the container as root:    docker exec -u root -it ${CONTAINER_ID} /bin/bash
+- To see output from Jenkins startup:   docker logs ${CONTAINER_ID}
+
 Waiting for Jenkins to start (should be ≈ 10 seconds)...
 EOF
 
