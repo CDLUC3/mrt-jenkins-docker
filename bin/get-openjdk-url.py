5bdc51432b4a56dc6a06e53ae8ef587d0bf33a4e
David Moles <david.moles@ucop.edu>
2019-03-05 16:00:38 -0800

    Add comments

--- bin/get-openjdk-url.py
+++ bin/get-openjdk-url.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+############################################################
+# get-openjdk-url.py
+#
+# Gets the URL for the latest OpenJDK release from
+# https://github.com/AdoptOpenJDK/openjdk-11-binaries
+############################################################
+
 import json
 import urllib2
 
@@ -11,21 +18,25 @@ JVM_HOTSPOT = "hotspot"
 def get_releases_json(releases_url=RELEASES_URL):
     opener = urllib2.build_opener()
     opener.addheaders = [
+        # Get the release information in JSON format
         ('Accept', 'application/vnd.github+json'),
-        ('User-Agent', 'curl/7.54.0')  # sketchy, I know
+        # GitHub doesn't like random Python scripts but it's OK with curl
+        # If this offends anybody, we could just shell out to curl :P
+        ('User-Agent', 'curl/7.54.0')
     ]
     response = opener.open(releases_url)
     return json.load(response)
 
 
 def find_latest_release(releases_json, arch=ARCH_X64_LINUX, jvm=JVM_HOTSPOT):
+    # Make sure we got the JSON we were expecting
     assert isinstance(releases_json, list), 'expected a JSON list, got ' + type(releases_json)
     for release in releases_json:
-        assert isinstance(release, dict), 'expected a JSON dict, got ' + type(release)
         if release["prerelease"]:
+            # Skip prerelease erleases
             continue
         for asset in release["assets"]:
-            assert isinstance(asset, dict), 'expected a JSON dict, got ' + type(asset)
+            # Find the right OS/architecture and JVM type
             asset_name = asset["name"]
             if arch + "_" + jvm in asset_name:
                 return asset["browser_download_url"]
