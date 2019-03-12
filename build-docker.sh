5ff7d94f7a3b48b1b66555251285b42d5b7174fb
David Moles <david.moles@ucop.edu>
2019-03-12 09:25:01 -0700

    Remove commented-out code

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#OPENJDK_11_RELEASE_JSON='https://raw.githubusercontent.com/AdoptOpenJDK/openjdk11-binaries/master/latest_release.json'
-#https://raw.githubusercontent.com/AdoptOpenJDK/openjdk8-binaries/master/latest_release.json
-
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd ${PROJECT_ROOT}
 docker build \
